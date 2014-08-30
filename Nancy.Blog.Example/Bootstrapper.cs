370e7a3dfd26a552d70cabcb7d62cd0271dab6aa
Mike Ward <mike@mike-ward.net>
2014-08-30 15:37:10 -0400

    Reading from files

--- Nancy.Blog.Example/Bootstrapper.cs
+++ Nancy.Blog.Example/Bootstrapper.cs
@@ -1,11 +1,31 @@
-﻿namespace Nancy.Blog.Example
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
+
+namespace Nancy.Blog.Example
 {
     using Nancy;
 
     public class Bootstrapper : DefaultNancyBootstrapper
     {
-        // The bootstrapper enables you to reconfigure the composition of the framework,
-        // by overriding the various methods and properties.
-        // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            var blog = new Blog();
+            var rootPath = container.Resolve<IRootPathProvider>().GetRootPath();
+
+            blog.Posts = Directory.GetFiles(Path.Combine(rootPath, "App_Data/Blog/"), "*.md")
+                .Select(file =>
+                {
+                    using (var f = File.OpenRead(file))
+                    {
+                        return Post.Read(f);
+                    }
+                })
+                .ToArray();
+
+            container.Register<IBlog>(blog);
+        }
     }
 }
--- Nancy.Blog/Blog.cs
+++ Nancy.Blog/Blog.cs
@@ -10,7 +10,7 @@ namespace Nancy.Blog
         public Blog()
         {
             Title = "Sample Blog";
-            Posts = new [] {new Post()};
+            Posts = new[] {new Post()};
         }
     }
 }
