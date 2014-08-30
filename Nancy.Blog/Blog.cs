1d6ff4450c72f88bf3dc20e6bdca60e1c58c59d2
Mike Ward <mike@mike-ward.net>
2014-08-30 13:55:25 -0400

    Sample article added

--- Nancy.Blog/Blog.cs
+++ Nancy.Blog/Blog.cs
@@ -1,7 +1,16 @@
-﻿namespace Nancy.Blog
+﻿using System.Collections.Generic;
+
+namespace Nancy.Blog
 {
     public class Blog : IBlog
     {
         public string Title { get; set; }
+        public IEnumerable<Post> Posts { get; set; }
+
+        public Blog()
+        {
+            Title = "Sample Blog";
+            Posts = new [] {new Post()};
+        }
     }
 }
--- Nancy.Blog/IBlog.cs
+++ Nancy.Blog/IBlog.cs
@@ -1,7 +1,10 @@
-﻿namespace Nancy.Blog
+﻿using System.Collections.Generic;
+
+namespace Nancy.Blog
 {
     public interface IBlog
     {
         string Title { get; set; }
+        IEnumerable<Post> Posts { get; set; }
     }
 }
