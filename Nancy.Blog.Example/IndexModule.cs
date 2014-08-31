698ac4be7b51bb6e954cacf8cfc76fdf34a20a22
Mike Ward <mike@mike-ward.net>
2014-08-31 18:18:58 -0400

    Add paging to sample

--- Nancy.Blog.Example/IndexModule.cs
+++ Nancy.Blog.Example/IndexModule.cs
@@ -1,6 +1,8 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.Linq;
 using Nancy.Blog.Example.Models;
+using Nancy.Responses.Negotiation;
 
 namespace Nancy.Blog.Example
 {
@@ -8,7 +10,8 @@ namespace Nancy.Blog.Example
     {
         public IndexModule(IndexModel model)
         {
-            Get["/"] = p => View[model];
+            Get["/"] = p => ShowBlog(model, 0);
+            Get["/{page:int}"] = p => ShowBlog(model, p.page);
             Get["/rss"] = p => model.Blog.Rss();
 
             Get["/archive"] = p => View["archive", model.Blog.Posts
@@ -20,6 +23,16 @@ namespace Nancy.Blog.Example
                         mg => CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(mg.Key),
                         mg => mg.OrderBy(d => d.Created)))
                 ];
+        }
+
+        private Negotiator ShowBlog(IndexModel model, int page)
+        {
+            const int pageLength = 3;
+            Context.ViewBag.PageLength = pageLength;
+            Context.ViewBag.Page = page;
+            Context.ViewBag.Prev = Math.Max(page - pageLength, 0);
+            Context.ViewBag.Next = Math.Min(model.Blog.Posts.Count() - 1, page + pageLength);
+            return View[model];
         }
     }
 }
