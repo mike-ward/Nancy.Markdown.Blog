e6f70d4f730ec3356aa71286601979e5ff196a85
Mike Ward <mike@mike-ward.net>
2014-09-07 13:00:13 -0400

    Refactor for readiblity

--- Nancy.Blog.Example/Modules/BlogModule.cs
+++ Nancy.Blog.Example/Modules/BlogModule.cs
@@ -12,29 +12,29 @@ namespace Nancy.Markdown.Blog.Example
         {
             Get["blog/"] = p => ShowBlog(model, 0);
             Get["blog/posts/{index:int}"] = p => ShowBlog(model, p.index) ?? 404;
-            Get["blog/post/{year:int}/{month:int}/{day:int}/{slug}"] = p => ShowArticle(model, p.year, p.month, p.day, p.slug) ?? 404;
+            Get["blog/post/{year:int}/{month:int}/{day:int}/{slug}"] = p => ShowPost(model, p.year, p.month, p.day, p.slug) ?? 404;
             Get["blog/archive"] = p => ShowArchive(model);
             Get["blog/rss"] = p => model.Blog.Rss();
         }
 
         private Negotiator ShowBlog(IBlogModel model, int index)
         {
-            const int numberOfPosts = 3;
-            var posts = model.Blog.Posts.Skip(index).Take(numberOfPosts);
+            const int postsToShow = 3;
+            var posts = model.Blog.Posts.Skip(index).Take(postsToShow);
             if (!posts.Any()) return null;
+            var prev = index - postsToShow;
+            var next = index + postsToShow;
             Func<int, string> link = p => string.Format("{0}/posts/{1}", model.Blog.BaseUri, p);
             ViewBag.Title = model.Blog.Title;
-            Context.ViewBag.Posts = posts;
-            var prev = index - numberOfPosts;
-            var next = index + numberOfPosts;
-            Context.ViewBag.Prev = prev >= 0 ? link(prev) : string.Empty;
-            Context.ViewBag.Next = next < model.Blog.Posts.Count() ? link(next) : string.Empty;
-            Context.ViewBag.PrevVisible = string.IsNullOrEmpty(Context.ViewBag.Prev) ? "hidden" : "visible";
-            Context.ViewBag.NextVisible = string.IsNullOrEmpty(Context.ViewBag.Next) ? "hidden" : "visible";
+            ViewBag.Posts = posts;
+            ViewBag.Prev = prev >= 0 ? link(prev) : string.Empty;
+            ViewBag.Next = next < model.Blog.Posts.Count() ? link(next) : string.Empty;
+            ViewBag.PrevVisible = string.IsNullOrEmpty(ViewBag.Prev) ? "hidden" : "visible";
+            ViewBag.NextVisible = string.IsNullOrEmpty(ViewBag.Next) ? "hidden" : "visible";
             return View[model.Blog];
         }
 
-        private Negotiator ShowArticle(IBlogModel model, int year, int month, int day, string slug)
+        private Negotiator ShowPost(IBlogModel model, int year, int month, int day, string slug)
         {
             var post = model.Blog.Posts
                 .FirstOrDefault(p =>
@@ -47,11 +47,11 @@ namespace Nancy.Markdown.Blog.Example
             var prev = model.Blog.PreviousPost(post);
             var next = model.Blog.NextPost(post);
             ViewBag.Title = post.Title;
-            Context.ViewBag.Posts = new[] {post};
-            Context.ViewBag.Prev = prev != null ? prev.PermaLink : string.Empty;
-            Context.ViewBag.Next = next != null ? next.PermaLink : string.Empty;
-            Context.ViewBag.PrevVisible = string.IsNullOrEmpty(Context.ViewBag.Prev) ? "hidden" : "visible";
-            Context.ViewBag.NextVisible = string.IsNullOrEmpty(Context.ViewBag.Next) ? "hidden" : "visible";
+            ViewBag.Posts = new[] {post};
+            ViewBag.Prev = prev != null ? prev.PermaLink : string.Empty;
+            ViewBag.Next = next != null ? next.PermaLink : string.Empty;
+            ViewBag.PrevVisible = string.IsNullOrEmpty(ViewBag.Prev) ? "hidden" : "visible";
+            ViewBag.NextVisible = string.IsNullOrEmpty(ViewBag.Next) ? "hidden" : "visible";
             return View[model.Blog];
         }
 
