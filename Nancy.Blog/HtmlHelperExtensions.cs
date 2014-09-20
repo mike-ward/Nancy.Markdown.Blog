b2e8a02f92bce7caab5437f28650f1b1b8ff51ed
mike-ward <mike@mike-ward.net>
2014-09-20 11:47:21 -0400

    Update HtmlHelperExtensions to take baseUri

--- Nancy.Blog/HtmlHelperExtensions.cs
+++ Nancy.Blog/HtmlHelperExtensions.cs
@@ -1,21 +1,20 @@
 ﻿using System.IO;
-using MarkdownDeep;
 using Nancy.ViewEngines.Razor;
 
 namespace Nancy.Markdown.Blog
 {
     public static class HtmlHelperExtensions
     {
-        public static IHtmlString Markdown<TModel>(this HtmlHelpers<TModel> helpers, string text)
+        public static IHtmlString Markdown<TModel>(this HtmlHelpers<TModel> helpers, string text, string baseUri = null)
         {
-            var md = new MarkdownDeep.Markdown {ExtraMode = true};
+            var md = new MarkdownDeep.Markdown {ExtraMode = true, UrlBaseLocation = baseUri};
             var html = md.Transform(text);
             return new NonEncodedHtmlString(html);
         }
 
-        public static IHtmlString MarkdownLoad<TModel>(this HtmlHelpers<TModel> helpers, string path)
+        public static IHtmlString MarkdownLoad<TModel>(this HtmlHelpers<TModel> helpers, string path, string baseUri = null)
         {
-            return helpers.Markdown(File.ReadAllText(path));
+            return helpers.Markdown(File.ReadAllText(path), baseUri);
         }
     }
 }
